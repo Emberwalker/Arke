0679f74263244673a536f954d7a674ce3898f3eb
Arkan Emberwalker <arkan@drakon.io>
2015-02-06 20:12:04 +0000

    Add more to admin, add __str__ to models.

--- arke/votes/admin.py
+++ arke/votes/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from votes.models import Vote
+from votes.models import Vote, Choice, CastVote
 
 admin.site.register(Vote)
+admin.site.register(Choice)
+admin.site.register(CastVote)
--- arke/votes/models.py
+++ arke/votes/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 from django.conf import settings
 
+
 class Vote(models.Model):
     question = models.CharField(max_length=512)
     extra_description = models.TextField(max_length=4096)
     submitter = models.ForeignKey(settings.AUTH_USER_MODEL)
 
+    def __str__(self):
+        return 'Vote[{}/{}]'.format(self.submitter, self.question)
+
+
 class Choice(models.Model):
     question = models.ForeignKey(Vote)
     choice_text = models.CharField(max_length=512)
 
+    def __str__(self):
+        return 'Choice[{}/{}]'.format(self.choice_text, self.question)
+
+
 class CastVote(models.Model):
     choice = models.ForeignKey(Choice)
     voter = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return 'CastVote[{}/{}]'.format(self.voter, self.choice)
